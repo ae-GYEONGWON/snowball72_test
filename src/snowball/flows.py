09840ee041f38d08d89fdf47174bf3442e89e1e7
ae-GYEONGWON <tjsxor50@gmail.com>
2025-03-07 18:15:44 +0900

    misc

--- src/snowball/flows.py
+++ src/snowball/flows.py
@@ -3,15 +3,11 @@ from sqlalchemy.orm import Session
 
 from src.snowball.models import Stock
 
-EXCEL_FILE_PATH = "src/snowball/백엔드 과제.xlsx"
-
-# 엑셀 데이터 불러오기
-df = pd.read_excel(EXCEL_FILE_PATH, sheet_name="예제")
-
 
 def load_excel_to_db(db: Session):
     """엑셀 파일에서 종가 데이터를 읽어 DB에 저장"""
     # ✅ 엑셀 데이터 불러오기
+    EXCEL_FILE_PATH = "src/snowball/백엔드 과제.xlsx"
     df = pd.read_excel(EXCEL_FILE_PATH, sheet_name="예제", header=1)
     df = df.iloc[:, :5]
     df = df.set_axis(["Date", "SPY", "QQQ", "GLD", "BIL"], axis=1)
